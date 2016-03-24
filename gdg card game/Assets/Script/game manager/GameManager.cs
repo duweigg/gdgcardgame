505d5e9111ecc360dad3a4caca14d49c594cca4d
Jason <jasonlimjingyong@gmail.com>
2016-03-24 21:12:22 +0800

    no message

--- gdg card game/Assets/Script/game manager/GameManager.cs
+++ gdg card game/Assets/Script/game manager/GameManager.cs
@@ -166,7 +166,7 @@ public class GameManager : MonoBehaviour {
 
             Position = mainUnit.transform.position + new Vector3(0, 0, 1); // position of the range
             Move_range_indicator.transform.localScale = new Vector3(2 * summonDistance, 1, 2 * summonDistance); //size of the range
-            Move_range_indicator.transform.position = Position; //place the range
+            Move_range_indicator.transform.position = Position ; //place the range
             
 
             Ray ray = cam.ScreenPointToRay(Input.mousePosition);
@@ -187,8 +187,10 @@ public class GameManager : MonoBehaviour {
                     if (_handcards[i] == _button) {
                         _handcards[i] = null;
                     }
+
                 }
                 arrangeCards();
+                Move_range_indicator.transform.position = new Vector3(0, 0, 0);
             }
             if (Input.GetMouseButton(1)) {
                 Destroy(_char);
@@ -227,7 +229,7 @@ public class GameManager : MonoBehaviour {
                         if (selectedScript.hasMoved == false) {
                             Position = selected.transform.position + new Vector3(0, -2.5f, 0); // position of the range
                             Move_range_indicator.transform.localScale = new Vector3(2 * maxMove, 1, 2 * maxMove); //size of the range
-                            Move_range_indicator.transform.position = Position; //place the range
+                            Move_range_indicator.transform.position = Position + new Vector3(0,4,0); //place the range
                         }
                         else {
                             Move_range_indicator.transform.position = new Vector3(0, 0, 0);
